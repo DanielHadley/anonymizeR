2038692e71042a2bbb49fb5b49dd35c1c50a8e79
Daniel Hadley <dphnrome@gmail.com>
2015-07-20 11:55:52 -0400

    added write_key

--- R/anonymize_randomly.R
+++ R/anonymize_randomly.R
@@ -4,21 +4,28 @@
 #' @param identifier A variable in the data that contains the characters or numbers you would like to replace with an anonymous ID. Should be wrapped in quotes.
 #' @param id_length The length of the newly generated random id. Defaults to 10.
 #' @param drop_old Do you want to drop the identifier from the data that gets returned? Defaults to TRUE.
+#' @param write_key Writes a csv file in the working directory with each unique identifier and its corresponding ID. Defaults to FALSE.
 #' @return A data frame identical to the original but with a new variable containing the random ID, which will be named <identifier>.anon.ID, and missing the identifier if drop_old is set to TRUE.
 #' @examples
 #' anonymize_randomly(mydata, "Social.Security", 9, FALSE)
 
-anonymize_randomly <- function(mydf, identifier, id_length = 10, drop_old = TRUE) {
+anonymize_randomly <- function(mydf, identifier, id_length = 10, drop_old = TRUE, write_key = FALSE) {
   # First group them using dplyr functions to find only the unique observations
-  # byIdentifier <- mydf %>% group_by_(identifier) %>% summarise
   byIdentifier <- dplyr::summarise(dplyr::group_by_(mydf, identifier))
   # Now create an random ID for each unique observation
   for(i in 1:nrow(byIdentifier)){
     byIdentifier$temp[i] <- paste(sample(c(0:9, letters, LETTERS), id_length, replace=TRUE), collapse="")
   }
   # Throw an error if two IDs are randomly the same
-  if(length(unique(byIdentifier$temp)) < length(byIdentifier$temp)) warning('Two or more randomly assigned IDs were the same. Please set.seed() to a different number, and/or increase the length of the ID generated.')
+  if(length(unique(byIdentifier$temp)) < length(byIdentifier$temp))
+    warning('Two or more randomly assigned IDs were the same. Please set.seed() to a different number, and/or increase the length of the ID generated.')
   colnames(byIdentifier)[2] <- paste0(identifier, ".anon.ID")
+  # Save a key in the working directory if TRUE
+  if (write_key) {
+    write.csv(byIdentifier,
+              paste0("./anonymizer_key_", identifier, ".csv"),
+              row.names=FALSE)
+  }
   # Join in back to the original dataset
   DeidentifiedData <- dplyr::left_join(mydf, byIdentifier)
   # Drop the identifier from original dataset if TRUE, or return joined data if FALSE
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # anonymizer
-Replaces Identifying Variables in a Data Frame
+Replaces Identifying Variables in a Data Frame with an anonymous ID. 
+
+## TODOs
+* Function that calls the key and adds new rows to the data frame for incrementally de-identifying 
+* Replace the data, identifier with just data$identifier 
+* Function to anonymize sequentially 
+* 
